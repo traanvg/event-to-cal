600d2ed262c40ef1e9c4bc553cee54ade9c310f0
Tran Vuong <tranvuong2906@gmail.com>
2026-06-24 22:08:29 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = "http://localhost:3001/api/parse";
+const API_URL = "https://event-to-cal.onrender.com/api/parse";
 
 function CalendarIcon() {
   return (
